fa414c4f9d7bacb3756e50dc0652c222cb41e93a
JoshNatYoyo <joshuaobrien28@yahoo.com>
2024-02-13 16:27:45 -0500

    To make sure master is correct

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const numbers = ['1, 2, 3, 4, 5, 6, 7, 8, 9, 0'];
+
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+
+
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
